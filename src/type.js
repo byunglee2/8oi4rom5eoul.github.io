bcd5cfacd92ab019afc1f6f63a39c421660b39c5
BSL <90385793+8oi4rom5eoul@users.noreply.github.com>
2023-10-09 19:59:25 +0900

    TypeIt

---
+++ src/type.js
@@ -0,0 +1,26 @@
+"use strict";
+
+new TypeIt(".home__title--strong", {
+  loop: true,
+  speed: 100,
+}) // Software Engineer|
+  .move(-18)
+  .type("Passionate ") //  Passionate |Software Engineer
+  .pause(1000)
+  .move(null, { to: "END" }) // Passionate Software Engineer|
+  .delete() // |
+  .type("Machine Learning Engineer")
+  .pause(1000)
+  .delete(25)
+  .type("Front-End Engineer")
+  .pause(1000)
+  .move(-9)
+  .delete(9)
+  .type("Back-End")
+  .pause(1000)
+  .delete(8)
+  .type("Full-Stack")
+  .pause(1000)
+  .move(9)
+  .delete()
+  .go();
