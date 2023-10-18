733c5cc85364594a6221f8c62c40da968bbca919
BSL <90385793+8oi4rom5eoul@users.noreply.github.com>
2023-10-17 23:02:52 -0400

    debugging1

--- src/active_menu.js
+++ src/active_menu.js
@@ -5,8 +5,6 @@
 // 보여지는 섹션:
 // - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
 // - 마지막 contact 섹션이 보여진다면, 그러면 가장 마지막 섹션을 선택
-"use strict";
-
 const sectionIds = [
   "#home",
   "#about",
@@ -15,11 +13,11 @@ const sectionIds = [
   "#testimonial",
   "#contact",
 ];
-
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
-  document.querySelector('[href="${id}"]')
+  document.querySelector(`[href="${id}"]`)
 );
+
 const visibleSections = sectionIds.map(() => false);
 let activeNavItem = navItems[0];
 
@@ -27,18 +25,18 @@ const options = {
   rootMargin: "-20% 0px 0px 0px",
   threshold: [0, 0.98],
 };
-const observer = new IntersectionObserver(callback, options);
+const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
 function observerCallback(entries) {
   let selectLastOne;
   entries.forEach((entry) => {
-    const index = sectionIds.indexOf("#${entry.target.id}");
+    const index = sectionIds.indexOf(`#${entry.target.id}`);
     visibleSections[index] = entry.isIntersecting;
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.IntersectionRatio >= 0.99;
+      entry.intersectionRatio >= 0.95;
   });
 
   const navIndex = selectLastOne
--- src/projects.js
+++ src/projects.js
@@ -2,7 +2,7 @@
 
 //프로젝트 필터링 관련 로직 처리
 const categories = document.querySelector(".categories");
-const projects = document.querySelectorAll;
+const projects = document.querySelectorAll(".project");
 const projectsContainer = document.querySelector(".projects");
 categories.addEventListener("click", (event) => {
   const filter = event.target.dataset.category;
@@ -22,7 +22,6 @@ function handleActiveSelection(target) {
 
 function filterProjects(filter) {
   //프로젝트 필터링 로직
-  projectsContainer.classList.add("anim-out");
   projects.forEach((project) => {
     if (filter === "all" || filter === project.dataset.type) {
       project.style.display = "block";
@@ -30,6 +29,7 @@ function filterProjects(filter) {
       project.style.display = "none";
     }
   });
+  projectsContainer.classList.add("anim-out");
   setTimeout(() => {
     projectsContainer.classList.remove("anim-out");
   }, 250);
